71a6b6f4b8959d1c824a41bc1e3b71a39347c3d8
Velryba <fernando,sabaiz@gmail.com>
2019-08-21 10:46:34 -0500

    Se incluye la pagina de compra, asi como los componentes de collection-preview y collection-item, con sus respectivos estilos.

--- src/App.js
+++ src/App.js
@@ -4,28 +4,17 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => ( <
-    div >
-    <
-    h1 > PAGINA DE SOMBREROS < /h1> < /
-    div >
-)
 
 function App() {
-    return ( <
-        div >
-        <
-        Switch >
-        <
-        Route exact path = '/'
-        component = { Homepage }
-        /> <
-        Route exact path = '/sombreros'
-        component = { HatsPage }
-        /> <
-        /Switch> < /
-        div >
+    return ( 
+    <div>
+        <Switch>
+            <Route exact path = '/' component = { Homepage } />
+            <Route exact path = '/shop' component = { ShopPage } />
+        </Switch> 
+    </div>
     );
 }
 
