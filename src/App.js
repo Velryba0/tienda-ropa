196db1c0379c2d41234239639589910d80f2a4af
Velryba <fernando,sabaiz@gmail.com>
2019-08-25 21:56:11 -0500

    Se agrego la pagina de carrito, el componente de carrito item y se actualizo carrito para usar selectores desde la libreria reselect.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sig-in-and-sign-up/sig-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './pages/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors'
 
 
 class App extends React.Component {
@@ -52,7 +56,11 @@ class App extends React.Component {
             /> <
             Route path = '/shop'
             component = { ShopPage }
-            />  <
+            /> <
+            Route exact path = '/checkout'
+            component = { CheckoutPage }
+            /> 
+             <
             Route exact path = '/signin'
             render = {
 
@@ -71,8 +79,8 @@ class App extends React.Component {
 
 }
 
-const mapStateToProps = ({ user }) => ({
-    currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
