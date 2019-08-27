f46504685a44f5ff88de7cd5c9edf327c2f7c261
Velryba <fernando,sabaiz@gmail.com>
2019-08-26 22:26:20 -0500

    Archivo adicional.

---
+++ src/config/keys.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyBRwAiG97EH_E3nPFo-JvHfdiye6JxlvOQ",
+    authDomain: "tienda-ropa-db.firebaseapp.com",
+    databaseURL: "https://tienda-ropa-db.firebaseio.com",
+    projectId: "tienda-ropa-db",
+    storageBucket: "",
+    messagingSenderId: "176719982300",
+    appId: "1:176719982300:web:545a1bcccd6b8a1a"
+};
+
+export default config;
